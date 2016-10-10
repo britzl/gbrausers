6723e3f63e8733c6ca65e8398d0b73bf2dbe256d
Björn Ritzl <bjorn.ritzl@king.com>
2016-10-10 07:46:18 +0200

    Reload, score, health

--- game/enemyplane.go
+++ game/enemyplane.go
@@ -14,6 +14,23 @@ components {
   }
 }
 embedded_components {
+  id: "bulletfactory"
+  type: "factory"
+  data: "prototype: \"/game/bullets/enemybullet.go\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
   id: "collisionobject"
   type: "collisionobject"
   data: "collision_shape: \"\"\n"
